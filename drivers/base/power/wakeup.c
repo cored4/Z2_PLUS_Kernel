8db8a04960fa941a7779e33052231641eef073e1
yarost12 <yaro330@gmail.com>
2018-03-02 14:07:10 +0300

    power: wakeup: force disable IPA_WS
    Signed-off-by: yarost12 <yaro330@gmail.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -24,7 +24,7 @@
 static bool enable_qcom_rx_wakelock_ws = true;
 static bool enable_wlan_extscan_wl_ws = true;
 static bool enable_wlan_wow_wl_ws = true;
-static bool enable_ipa_ws = true;
+static bool enable_ipa_ws = false;
 static bool enable_wlan_ws = true;
 static bool enable_timerfd_ws = true;
 static bool enable_netlink_ws = true;
@@ -44,7 +44,7 @@ static bool enable_alarmtimer_ws = false;
 module_param(enable_qcom_rx_wakelock_ws, bool, 0644);
 module_param(enable_wlan_extscan_wl_ws, bool, 0644);
 module_param(enable_wlan_wow_wl_ws, bool, 0644);
-module_param(enable_ipa_ws, bool, 0644);
+module_param(enable_ipa_ws, bool, 0444);
 module_param(enable_wlan_ws, bool, 0644);
 module_param(enable_timerfd_ws, bool, 0644);
 module_param(enable_netlink_ws, bool, 0644);
